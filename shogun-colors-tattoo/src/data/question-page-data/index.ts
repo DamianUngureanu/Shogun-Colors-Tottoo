24a7a93a2c66e7d063d8bc3f237e9177369a1905
DamianU <damianungureanu2000@gmail.com>
2025-07-16 16:30:40 +0300

    Auto deploy

--- shogun-colors-tattoo/src/data/question-page-data/index.ts
+++ shogun-colors-tattoo/src/data/question-page-data/index.ts
@@ -4,21 +4,17 @@ import startingImage from "@/public/background-images/question-page-background.j
 export const QuestionPageData: QuestionPageType = {
   startingImage: startingImage,
   questions: [
-    {
-      titleRo: "Ce este un tatuaj?",
-      titleEn: "What is a tattoo?",
-      descriptionRo:
-        "Un tatuaj este o formă de artă corporală care implică introducerea de cerneală în piele pentru a crea un design permanent",
-      descriptionEn:
-        "A tattoo is a form of body art that involves inserting ink into the skin to create a permanent design, often used for self-expression or cultural significance.",
-    },
-    {
-      titleRo: "Cum se îngrijesc tatuajele?",
-      titleEn: "How to care for tattoos?",
-      descriptionRo:
-        "Îngrijirea tatuajelor este esențială pentru a asigura o vindecare adecvată și pentru a menține aspectul acestora. Este important să urmezi instrucțiunile artistului tatuator, care pot include curățarea regulată, aplicarea de unguente și evitarea expunerii la soare.",
-      descriptionEn:
-        "Caring for tattoos is essential to ensure proper healing and maintain their appearance. It is important to follow the tattoo artist's instructions, which may include regular cleaning, applying ointments, and avoiding sun exposure.",
-    },
-  ],
+  {
+    "titleRo": "Cum se îngrijesc tatuajele?",
+    "titleEn": "How to care for tattoos?",
+    "descriptionRo": "Îngrijirea tatuajelor este esențială pentru a asigura o vindecare adecvată și pentru a menține aspectul acestora. Este important să urmezi instrucțiunile artistului tatuator, care pot include curățarea regulată, aplicarea de unguente și evitarea expunerii la soare.",
+    "descriptionEn": "Caring for tattoos is essential to ensure proper healing and maintain their appearance. It is important to follow the tattoo artist's instructions, which may include regular cleaning, applying ointments, and avoiding sun exposure."
+  },
+  {
+    "titleRo": "",
+    "titleEn": "",
+    "descriptionRo": "",
+    "descriptionEn": ""
+  }
+]
 };
