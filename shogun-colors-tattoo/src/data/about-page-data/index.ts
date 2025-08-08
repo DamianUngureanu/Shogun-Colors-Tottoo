558a4c1a5340d57e45ea9a5072dbeacbdfe66082
DamianU <damianungureanu2000@gmail.com>
2025-08-08 21:11:54 +0300

    Last commit 3

--- shogun-colors-tattoo/src/data/about-page-data/index.ts
+++ shogun-colors-tattoo/src/data/about-page-data/index.ts
@@ -1,11 +1,11 @@
 import AboutPageType from "@/types/about-page-type";
-import img1 from "@/public/about-images/1-about.jpeg";
+import img2 from "@/public/about-images/2-about.jpeg";
 
 const AboutPageData: AboutPageType = {
   backgroundImage: undefined,
   about: [
     {
-        image: img1,
+        image: undefined,
         titleRO: "Shogun",
         titleEN: "Shogun",
         descriptionRO: "Născut în Brașov și pasionat de desen încă din copilărie, am transformat arta într-un mod de viață. De la primele graffiti-uri din tinerețe, până la primele linii trasate pe piele, drumul meu a fost mereu ghidat de creativitate. Astăzi, la 40 de ani și cu peste 16 ani de experiență în tatuaje, sunt mândru să fac parte din Uniunea Tatuatorilor Români (UTR) și să fiu premiat la convenții de tatuaje pentru munca mea.  Am urmat cursuri și seminarii de specialitate, mereu atent la evoluția artei tatuajului. Ce iubesc cel mai mult la această meserie este că îmi oferă ocazia să cunosc oameni noi, să le ascult poveștile și să le ofer ceva care rămâne – un tatuaj cu semnificație, ce dăinuie în timp.",
@@ -13,7 +13,7 @@ const AboutPageData: AboutPageType = {
         revers: false
       },
     {
-        image: undefined,
+        image: img2,
         titleRO: "Studio Shogun",
         titleEN: "Shogun's Studio",
         descriptionRO: "Situat în inima Brașovului, studioul meu este deschis de peste 4 ani și a fost gândit ca un spațiu personal, intim și diferit de un salon clasic de tatuaje. Este un studio complet echipat cu aparatură de ultimă generație, folosind ace sterile, tușuri și mașini de tatuat profesionale, pentru a oferi o experiență sigură și de calitate.  Atmosfera este caldă și primitoare – pereții studioului sunt decorați cu accesorii și suveniruri din toată lumea, primite sau aduse de clienți fideli de-a lungul anilor, ceea ce transformă locul într-un spațiu cu suflet, nu doar un atelier de lucru.  Igiena și ordinea sunt o prioritate absolută, iar clienții simt asta din primul moment. Mulți spun că e un loc care iese din tipare – un studio captivant și autentic, apreciat nu doar de clienți, ci și de colegii din breaslă.",
